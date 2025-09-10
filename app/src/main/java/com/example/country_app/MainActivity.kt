e0bfb5ebc54aa43e5075f502d785e485855eb2f1
aarmeliaa <azzahraarmrlia14@gmail.com>
2025-09-10 14:01:26 +0700

    mencoba spinner untuk dropdown hihi aku ikutin modul aja :3

---
+++ app/src/main/java/com/example/country_app/MainActivity.kt
@@ -0,0 +1,68 @@
+package com.example.country_app
+
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.country_app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var provinces: Array<String>
+    private val countries = arrayOf(
+        "Indonesia",
+        "United States",
+        "United Kingdom",
+        "Germany",
+        "France",
+        "Australia",
+        "Japan",
+        "China",
+        "Brazil",
+        "Canada"
+    )
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        provinces = resources.getStringArray(R.array.provinces)
+        with(binding){
+            val adapterCountry = ArrayAdapter(
+                this@MainActivity,
+                android.R.layout.simple_spinner_item, countries
+            )
+
+            adapterCountry.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerCountry.adapter = adapterCountry
+
+            spinnerCountry.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(
+                        parent: AdapterView<*>,
+                        view: View, position: Int, id: Long
+                    ) {
+                        Toast.makeText(
+                            this@MainActivity,
+                            countries[position], Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                    override fun onNothingSelected(parent: AdapterView<*>) {
+                        // write code to perform some action
+                    }
+                }
+
+
+            val adapterProvinces = ArrayAdapter(this@MainActivity,
+                android.R.layout.simple_spinner_item, provinces)
+
+            adapterProvinces.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerProvinces.adapter = adapterProvinces
+
+        }
+    }
+}
